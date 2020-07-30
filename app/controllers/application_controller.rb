48f5fdb6bc9e8379f2ac5911d971bec595d1b9d3
Jonathan East <jonathan_east@ymail.com>
2020-07-30 19:40:45 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,18 @@ class App < Sinatra::Base
     end
     
     post '/teams' do
-      
+      @team_name = params[:team][:name]
+      @team_motto = params[:team][:motto]
+      @hero_name = []
+      @hero_power = []
+      @hero_bio = []
+      @team_members = params[:team][:members]
+      @team_members.each do |team_member|
+        @hero_name << team_member[:name]
+        @hero_power << team_member[:power]
+        @hero_bio << team_member[:bio]
+      end
+      erb :team
     end
 
 end
